1543bfe19f707280db272afbc815ccda0c0935ef
specnazm <laketic.milena98@gmail.om>
2021-11-09 14:08:51 +0100

    auto load env

--- testingapp/__init__.py
+++ testingapp/__init__.py
@@ -6,6 +6,9 @@ from datetime import timedelta
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
--- testingapp/routes/subject.py
+++ testingapp/routes/subject.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, Response
-from testingapp import db, rbac
+from testingapp import db
 from testingapp.models.testmodels import Subject
 from testingapp.models.usermodels import User
 from flask_jwt_extended import jwt_required, get_jwt_identity
