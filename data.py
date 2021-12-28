cda3bcdbee36189d70d9ba7619faa0c16e27af65
Milena Laketic <laketic.milena98@gmail.com>
2021-12-28 13:56:16 +0100

    Changed data.py. 1 test,1 part,5 sec

--- data.py
+++ data.py
@@ -6,18 +6,20 @@ import random
 
 fake = Faker()
 emails = [fake.unique.email() for i in range(50)]
+emails.insert(0, 'prof@gmail.com')
+emails.insert(4, 'student@gmail.com')
 teachers = []
 students = []
 subjects = []
 
 
-# db.drop_all()
-# db.create_all()
+db.drop_all()
+db.create_all()
 
 
 def add_admin():
     admin = Admin(
-        email=emails[0], 
+        email=emails[40], 
         first_name=fake.first_name(),
         last_name=fake.last_name(),
         password_hash=flask_bcrypt.generate_password_hash("123456").decode('utf-8')
@@ -26,7 +28,7 @@ def add_admin():
     db.session.commit()
 
 def add_teachers():
-    for i in range(1, 10):
+    for i in range(0, 3):
         teacher = Teacher(
             email=emails[i], 
             first_name=fake.first_name(), 
@@ -39,7 +41,7 @@ def add_teachers():
         teachers.append(teacher)
 
 def add_students():
-    for i in range(11, 20):
+    for i in range(4, 14):
         student = Student(
             email=emails[i], 
             first_name=fake.first_name(), 
@@ -59,49 +61,45 @@ def add_subjects():
             description=fake.paragraph(),
             points=random.choice(range(2,10)) 
             )
-        teacher_index = random.choice(range(0, len(teachers)))
-        subject.teachers.append(teachers[teacher_index])
+        subject.teachers.append(teachers[0])
         for stud in students:
             subject.students.append(stud)
         db.session.add(subject)
         db.session.commit()
         subjects.append(subject)
 
-
+#one test example for subject with id 0
 def add_tests():
-
-    for i in range(3):
-        time_dependency = fake.boolean(chance_of_getting_true=50)
-        test = Test(
-            title=fake.text(max_nb_chars=20), 
-            time_dependency=time_dependency,
-            time_limit_seconds=(random.choice([900, 1800, 2700]) if time_dependency else 0),
-            )
-        subject_index = random.choice(range(0, len(subjects)))
-        test.subject = subjects[subject_index]
-        test.subject_id = subjects[subject_index].id
-        db.session.add(test)
-        db.session.commit()
-        add_parts(test)
+    time_dependency = fake.boolean(chance_of_getting_true=50)
+    test = Test(
+        title=fake.text(max_nb_chars=20), 
+        time_dependency=time_dependency,
+        time_limit_seconds=(random.choice([900, 1800, 2700]) if time_dependency else 0),
+        )
+    test.subject = subjects[0]
+    test.subject_id = subjects[0].id
+    db.session.add(test)
+    db.session.commit()
+    add_parts(test)
 
     return test
         
-
+#one part for given test
 def add_parts(test):
-    for i in range(3):
-        part = Part(
-            title=fake.text(max_nb_chars=20), 
-            navigation_mode=random.choice([e.name for e in NavigationMode]),
-            submission_mode=random.choice([e.name for e in SubmissionMode]),
-            )
-        db.session.add(part)
-        test.parts.append(part)
-        db.session.commit()
+    part = Part(
+        title=fake.text(max_nb_chars=20), 
+        navigation_mode=random.choice([e.name for e in NavigationMode]),
+        submission_mode=random.choice([e.name for e in SubmissionMode]),
+        )
+    db.session.add(part)
+    test.parts.append(part)
+    db.session.commit()
 
-        add_sections(part.id)
+    add_sections(part.id)
 
+#5 sections for given part
 def add_sections(part_id):
-    for i in range(2):
+    for i in range(5):
         section = Section(
             title=fake.text(max_nb_chars=10), 
             part_id=part_id
@@ -111,9 +109,10 @@ def add_sections(part_id):
 
         add_items(section.id)
 
+#3 items for given section
 def add_items(section_id):
     for i in range(3):
-        max_choices=random.choice([1, 1, 3])
+        max_choices=random.choice([1, 1, 2])
         item = Item(
             score=random.choice(range(2,5)), 
             question=fake.text(max_nb_chars=20) + " ?",
@@ -125,10 +124,10 @@ def add_items(section_id):
 
         add_options(item.id, max_choices)
 
-
+#3 options for each item(question)
 def add_options(item_id, max_choices):
-    option_names = ['A', 'B', 'C', 'D', 'E']
-    for i in range(5):
+    option_names = ['A', 'B', 'C']
+    for i in range(3):
         option = Option(
             label=fake.text(max_nb_chars=30),
             name=option_names[i],
@@ -139,9 +138,8 @@ def add_options(item_id, max_choices):
         db.session.commit()
         max_choices = max_choices - 1
 
-#add test result for one test 
-def add_test_result():
-    test = Test.query.get(1)
+#simulate 3 student's answers to given test
+def add_test_result(test):
     subject = test.subject
     student1 = subject.students[0]
     student2 = subject.students[1]
@@ -162,15 +160,15 @@ def add_test_result():
 
 
 if __name__ == '__main__':
-    # add_admin()
-    # print("\nADDED ADMIN....\n")
-    # add_teachers()
-    # print("\nADDED TEACHERS....\n")
-    # add_students()
-    # print("\nADDED STUDENTS....\n")
-    # add_subjects()
-    # print("\nADDED SUBJECTS....")
-    # test = add_tests()
-    # print("\nADDED TESTS....\n")
-    add_test_result()
+    add_admin()
+    print("\nADDED ADMIN....\n")
+    add_teachers()
+    print("\nADDED TEACHERS....\n")
+    add_students()
+    print("\nADDED STUDENTS....\n")
+    add_subjects()
+    print("\nADDED SUBJECTS....")
+    test = add_tests()
+    print("\nADDED TESTS....\n")
+    add_test_result(test)
     print("\nADDED TEST RESULTS....\n")
